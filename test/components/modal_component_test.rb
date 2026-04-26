ebf4514146b28ace7e0cd91d37bd3f2e8190164e
WeiHsi Hu <walzerfan@yahoo.com>
2026-04-26 12:58:22 +0200

    Add failing tests for ModalComponent template

--- test/components/modal_component_test.rb
+++ test/components/modal_component_test.rb
@@ -3,10 +3,23 @@
 require "test_helper"
 
 class ModalComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(ModalComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  test "renders a hidden root with the modal Stimulus controller" do
+    render_inline(ModalComponent.new)
+    assert_selector "[data-controller='modal'][data-modal-target='root'].hidden", visible: :all
+  end
+
+  test "renders an empty turbo-frame named modal as the body slot" do
+    render_inline(ModalComponent.new)
+    assert_selector "turbo-frame#modal[data-modal-target='frame']", visible: :all
+  end
+
+  test "renders a close button with an aria-label" do
+    render_inline(ModalComponent.new)
+    assert_selector "button[type='button'][aria-label][data-action*='modal#close']", visible: :all
+  end
+
+  test "backdrop closes the modal" do
+    render_inline(ModalComponent.new)
+    assert_selector "[data-action='click->modal#close'].bg-black\\/40", visible: :all
   end
 end
