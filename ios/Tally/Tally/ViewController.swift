31e6a47d946fe84de18e6262b926330ca0b77940
WeiHsi Hu <walzerfan@yahoo.com>
2026-05-01 12:38:34 +0200

    Scaffold Xcode project with Hotwire Native dependency

---
+++ ios/Tally/Tally/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tally
+//
+//  Created by WeiHsi Hu on 01.05.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
